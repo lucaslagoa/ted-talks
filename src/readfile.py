256b51493a6b9d3c315e9467dcf68716b899d9e1
Luan Gonçalves <luanlg.cco@gmail.com>
2018-06-17 14:49:47 -0300

    Fix ReadFile

--- src/readfile.py
+++ src/readfile.py
@@ -8,19 +8,25 @@ class ReadFile():
     def __init__(self, filename):
         self.filename = filename
         self.ted_talks = {'title': pd.read_csv(filename)['title'],
-                          'related_talks': self.__parse('related_talks')}
+                          'related_talks': self.__parse('related_talks'),
+                          'ratings': self.__parse('ratings')}
 
         self.n_nodes = len(self.ted_talks['title'])
 
     def __parse(self, name):
         data = pd.read_csv(self.filename)[name]
-
-        last_index = len(data[0])-1
-        data = data[0][1:last_index].split('},')
         result = []
-        for d in data:
-            if d[-1] != '}':
-                d = d+"}"
-            result.append(ast.literal_eval(d.strip()))
 
+        for d in data:
+            d = d[1:-1].split('},')
+            rows = []
+
+            for value in d:
+                if value[-1] != '}':
+                    value = value + '}'
+
+                value = value.strip()
+                rows.append(ast.literal_eval(value))
+
+            result.append(rows)
         return result
